7c5ee6fe72a55253a0874b38d2a1e8934a08575d
Dexter <dexter@dexterlabs.nl>
2019-11-14 16:17:05 +0100

    Setup 'backend' (hasura + postgres)

---
+++ sql/00.init.sql
@@ -0,0 +1,26 @@
+-- CREATE SCHEMA public;
+DROP TABLE IF EXISTS public.todos CASCADE;
+
+CREATE OR REPLACE FUNCTION random_string(length INTEGER) returns TEXT AS
+$$
+declare
+  chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+  result text := '';
+  i integer := 0;
+begin
+  if length < 0 then
+    raise exception 'Given length cannot be less than 0';
+  end if;
+  for i in 1..length loop
+    result := result || chars[1+random()*(array_length(chars, 1)-1)];
+  end loop;
+  return result;
+end;
+$$ language plpgsql;
+
+CREATE TABLE public.todos (
+  id                VARCHAR(16)         DEFAULT random_string(16),
+  data              JSONB               DEFAULT '{}',
+
+  PRIMARY KEY (id)
+);
