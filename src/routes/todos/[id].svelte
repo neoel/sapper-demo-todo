64e7bc60778d2a0ff9ff2a9755735fed66daaee5
Dexter <dexter@dexterlabs.nl>
2019-11-14 17:56:33 +0100

    Storing mutated todo's

--- src/routes/todos/[id].svelte
+++ src/routes/todos/[id].svelte
@@ -1,6 +1,6 @@
 <script context=module>
   import gql from 'graphql-tag'
-  import { query, awaitQueries } from '../../graphql'
+  import { query, mutation, awaitQueries } from '../../graphql'
 
   const getTodo = id => query({
     query: gql`
@@ -17,19 +17,34 @@
     const { id } = page.params
 
     return awaitQueries({
+      id,
       todoStore: getTodo(id)
     })
   }
 </script>
 
 <script>
+
   import Todo from '../../components/Todo.svelte'
 
   export let id
   export let todoStore
 
+  const updateTodo = mutation({
+    mutation: gql`
+      mutation updateTodo($id: String!, $data: jsonb!) {
+        update_todos(where:{id:{_eq:$id}}, _set:{data:$data}) {
+          affected_rows
+        }
+      }
+    `
+  })
+
   $: todos = $todoStore.data.todos_by_pk.data
 </script>
 
 
-<Todo {todos} />
+<Todo
+  {todos}
+  on:save={({ detail: data }) => updateTodo({ id, data})}
+/>
